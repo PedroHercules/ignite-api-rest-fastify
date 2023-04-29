b5409ecedf56b244cc20ed007395b2f5b920ef99
PedroHercules <pedroherculesdantas@gmail.com>
2023-04-28 22:45:43 -0300

    test: created first e2e test

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,8 +1,26 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('O usuário consegue criar uma nova transação', () => {
+// Antes de executar todos os testes
+beforeAll(async () => {
+  // Aguardar todos os plugins estarem prontos
+  await app.ready()
+})
+
+// Depois de executar todos os testes
+afterAll(async () => {
+  // Fechar a aplicação, remover da memória
+  await app.close()
+})
+
+test('User can create a new transaction', async () => {
   // Fazer uma requisição HTTP para criar uma transação
-  const responseStatus = 201
+  const response = await request(app.server).post('/transactions').send({
+    title: 'New transaction test',
+    amount: 5000,
+    type: 'credit',
+  })
 
-  expect(responseStatus).toEqual(201)
+  expect(response.statusCode).toEqual(201)
 })
