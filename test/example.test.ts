1f124bd19e09f1774b35ae56650d12286817a13f
PedroHercules <pedroherculesdantas@gmail.com>
2023-04-28 23:11:24 -0300

    test(transactions): created test to return all transactions

--- test/example.test.ts
+++ test/example.test.ts
@@ -15,6 +15,13 @@ describe('Transaction routes', () => {
     await app.close()
   })
 
+  /**
+   * The method it have some methods
+   * skip: skip a test
+   * todo: check a test with todo
+   * only: execute only test with this method
+   */
+
   it('should be able to create a new transaction', async () => {
     // Fazer uma requisição HTTP para criar uma transação
     const response = await request(app.server).post('/transactions').send({
@@ -24,5 +31,29 @@ describe('Transaction routes', () => {
     })
 
     expect(response.statusCode).toEqual(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction test',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction test',
+        amount: 5000,
+      }),
+    ])
   })
 })
