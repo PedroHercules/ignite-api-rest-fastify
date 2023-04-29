bd986b9fdf3f1f7ee31af54ec1ef40a54a83549f
PedroHercules <pedroherculesdantas@gmail.com>
2023-04-28 22:54:49 -0300

    test(transaction): added a describe method

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,26 +1,28 @@
-import { afterAll, beforeAll, expect, test } from 'vitest'
+import { afterAll, beforeAll, expect, it, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-// Antes de executar todos os testes
-beforeAll(async () => {
-  // Aguardar todos os plugins estarem prontos
-  await app.ready()
-})
-
-// Depois de executar todos os testes
-afterAll(async () => {
-  // Fechar a aplicação, remover da memória
-  await app.close()
-})
-
-test('User can create a new transaction', async () => {
-  // Fazer uma requisição HTTP para criar uma transação
-  const response = await request(app.server).post('/transactions').send({
-    title: 'New transaction test',
-    amount: 5000,
-    type: 'credit',
+describe('Transaction routes', () => {
+  // Antes de executar todos os testes
+  beforeAll(async () => {
+    // Aguardar todos os plugins estarem prontos
+    await app.ready()
   })
 
-  expect(response.statusCode).toEqual(201)
+  // Depois de executar todos os testes
+  afterAll(async () => {
+    // Fechar a aplicação, remover da memória
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    // Fazer uma requisição HTTP para criar uma transação
+    const response = await request(app.server).post('/transactions').send({
+      title: 'New transaction test',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
