e528e950051c61b3e5e77e2daaec762af058e050
PedroHercules <pedroherculesdantas@gmail.com>
2023-04-29 23:22:22 -0300

    chore: configured to production deploy

--- src/env/index.ts
+++ src/env/index.ts
@@ -13,7 +13,10 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  // Coerce converts any value to a type (in this case number)
+  // If is not valid, use the default value 3333
+  // This is because the app render pass a PORT env as string to deploy
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
